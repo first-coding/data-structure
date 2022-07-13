a1558d45caf95943df592e665e72eacb440dd76a
first-coding <1690634875@qq.com>
2022-07-13 16:41:31 +0800

    July 13

--- Sort ChoiceAlgorithm/Sort.c
+++ Sort ChoiceAlgorithm/Sort.c
@@ -1,9 +1,14 @@
 #include "Sort.h"
 
 int main() {
+	clock_t start, stop;
 	int s[10] = { 10,2,3,88,22,10,30,80,90,20 };
-	ThreeDivideSort(s, 0, 9);
+	start = clock();
+	sort(s, 0, 9);
 	ItemShows(s, 10);
+	stop = clock();
+	double duration = ((double)(stop - start)) / CLK_TCK;
+	printf("%f\n", duration);
 	return 0;
 }
 
