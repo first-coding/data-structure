688a7367164ca08098d1e71b92385cf94c93f11a
first-coding <1690634875@qq.com>
2022-07-31 18:59:06 +0800

    July 31

--- set/BitVector.c
+++ set/BitVector.c
@@ -2,11 +2,15 @@
 
 int main() {
 	Set S = SetInit(20);
+	Set S2 = SetInit(20);
 	for (int i = 0; i < 20; i++) {
 		Insert(i, i + 1, S);
+		Insert(i, i + 5, S2);
 	}
-	for (int i = 0; i < 20; i++) {
-		printf("%d\n", S->v[i]);
-	}
+	//for (int i = 0; i < 20; i++) {
+	//	printf("%d\n", S->v[i]);
+	//}
+	Set tmp = SetUnion(S, S2);
+	printf("%d\n", tmp->v[1]);
 	return 0;
 }
