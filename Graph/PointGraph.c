a12831128fd0fd7e55003af5b0dd537e35609c11
first-coding <1690634875@qq.com>
2022-08-21 17:40:37 +0800

    Aug 21

--- Graph/PointGraph.c
+++ Graph/PointGraph.c
@@ -1,37 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #pragma warning(disable:4996)
-#define MVNum 100     
-typedef struct ArcNode* arcnode;
-typedef struct ArcNode {                		
-    int adjvex;                                   
-    arcnode nextarc;       
+#pragma warning(disable:6031)
+#define MVNum 100                              
+typedef struct ArcNode {
+	int adjvex;
+	struct ArcNode* nextarc;
 }ArcNode;
 
 typedef struct VNode {
-    char data;                    	     
-    arcnode firstarc;        
-}VNode, AdjList[MVNum];             
+	char data;
+	ArcNode* firstarc;
+}VNode, AdjList[MVNum];
 
 typedef struct {
-    AdjList vertices;             
-    int vexnum, arcnum;    
+	AdjList vertices;
+	int vexnum, arcnum;
 }ALGraph;
 
-void PointGraph(ALGraph *G) {
-	//ArcNode* s;
-	printf("please input TopNode and SideNode");
+void CreatMGraph(ALGraph* G);
+void printGraph(ALGraph G);
+
+void CreatMGraph(ALGraph* G)
+{
+	int i, j, k;
+	ArcNode* s;
 	scanf("%d%d", &G->vexnum, &G->arcnum);
-	for (int i = 0; i < G->vexnum; i++) {   		
-		scanf("%c", &G->vertices[i].data);  
+	for (i = 0; i < G->vexnum; i++) {
+		scanf(" %c", &G->vertices[i].data);
 	}
-	for (int i = 0; i < G->vexnum; i++) {
-		G->vertices[i].firstarc = NULL;   	
+	for (i = 0; i < G->vexnum; i++) {
+		G->vertices[i].firstarc = NULL;
+	}
+	for (k = 0; k < G->arcnum; k++) {
+		scanf("%d%d", &i, &j);
+		s = (ArcNode*)malloc(sizeof(ArcNode));
+		s->adjvex = j;
+		s->nextarc = G->vertices[i].firstarc;
+		G->vertices[i].firstarc = s;
+		s = (ArcNode*)malloc(sizeof(ArcNode));
+		s->adjvex = i;
+		s->nextarc = G->vertices[j].firstarc;
+		G->vertices[j].firstarc = s;
+	}
+}
+void printGraph(ALGraph G)
+{
+	int i, j;
+	ArcNode* p;
+	for (i = 0; i < G.vexnum; i++) {
+		printf("%c:", G.vertices[i].data);
+		for (p = G.vertices[i].firstarc; p; p = p->nextarc) {
+			printf(" %d", p->adjvex);
+		}
+		printf("\n");
 	}
 }
 
-int main() {
+
+int main()
+{
 	ALGraph G;
-	PointGraph(&G);
+	CreatMGraph(&G);
+	printGraph(G);
 	return 0;
 }
